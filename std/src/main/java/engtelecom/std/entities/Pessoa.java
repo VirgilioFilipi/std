a26fafc27ba334317fe5d52c270c8be9c9fc0177
Filipi Virgilio <108130900+VirgilioFilipi@users.noreply.github.com>
2023-11-13 09:20:55 -0300

    v2

--- std/src/main/java/engtelecom/std/entities/Pessoa.java
+++ std/src/main/java/engtelecom/std/entities/Pessoa.java
@@ -2,15 +2,15 @@ package engtelecom.std.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Pessoa {
+
 
     private Long id;
     private String nome;
-    private String email;
-
-
-    
+    private String email;    
 }
--- std/src/main/java/engtelecom/std/service/PessoaService.java
+++ std/src/main/java/engtelecom/std/service/PessoaService.java
@@ -17,6 +17,10 @@ public class PessoaService {
     public PessoaService(){
         this.agenda = new ArrayList<>();
         this.contador = new AtomicLong();    
+        // adicionando algumas pessoas para facilitar os testes
+        this.cadastrarPessoa(new Pessoa(null, "João", "juca@"));
+        this.cadastrarPessoa(new Pessoa(null, "Maria", "ana@aluno"));
+            
        }
 
     public List<Pessoa> buscarTodas(){
@@ -30,5 +34,30 @@ public class PessoaService {
         this.agenda.add(p);
         return p;
     }
+
+    public Pessoa buscarPorId(Long id) {
+        for (Pessoa p : agenda){
+            if (p.getId() == id){
+                return p;
+            }
+        }
+        return null;
+    }
     
+public Pessoa atualizar(Pessoa pessoa) {
+Pessoa p = buscarPorId(pessoa.getId());
+if (p != null) {
+p.setNome(pessoa.getNome());
+
+}
+return p;
+}
+public boolean excluir(Long id) {
+Pessoa p = buscarPorId(id);
+if (p != null) {
+this.agenda.remove(p);
+return true;
+}
+return false;
+}
 }
